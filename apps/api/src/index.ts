5e67f80bd647400cc1209550c16efc82034a79bf
Armstrong Prince <officialnimi@gmail.com>
2025-09-29 18:43:52 +0100

    Implemented get user profile and update email

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,14 +1,19 @@
 import express, { Request, Response } from 'express';
+import router from './routes/userRoutes';
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
 
+app.use("/api/users", router);
+
 app.get('/api/health', (req: Request, res: Response) => {
   res.json({ message: 'Qyou API is healthy!' });
 });
 
+
 app.listen(port, () => {
   console.log(`[api]: Server is running at http://localhost:${port}`);
 });
+
